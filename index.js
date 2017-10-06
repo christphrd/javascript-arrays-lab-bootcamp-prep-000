d05f61dabcaf906be44fb5ea8af2901c2ce52754
Christopher Diep <diep.christopher@gmail.com>
2017-10-06 03:30:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(name) {
+  return kitten.pop()
+}
