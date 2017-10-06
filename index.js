d1926bfa5f5e936b27b550fe808438c7c3444d8d
Christopher Diep <diep.christopher@gmail.com>
2017-10-06 03:35:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   return [...kittens, name]
 }
+
+function prependKitten(name) {
+  return [name, ...kittens]
+}
+
+function removeLastKitten(name) {
+  return kittens.slice(0,kittens.length-1)
+}
